2cf1e707a1d993b4b0573361954f1ed3c993cbd2
shivanshvermaa <shivansh.verma8080@gmail.com>
2019-05-29 23:00:51 +0530

    INITIAL COMMIT

---
+++ hompage.php
@@ -0,0 +1,77 @@
+<html>
+
+<head>
+    <title></title>
+
+    <link rel="stylesheet" href="css/nav.css" type="text/css">
+    <link rel="stylesheet" href="css/styles.css" type="text/css">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+
+
+
+
+</head>
+
+<body>
+
+    <header>
+
+        <div class="container">
+
+            <div id="headerlogo">
+                <h1>THE ROAD RUNNERS</h1>
+            </div>
+
+            <nav>
+                <ul>
+                    <li class="current"><a href="#" class="c">Home</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Cars</a></li>
+                    <li><a href="#">Events</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">Achievements</a></li>
+                    <li><a href="#">Support</a></li>
+                    <li><a href="#">Team</a></li>
+                    <li><a href="#">Alumni</a></li>
+                </ul>
+            </nav>
+
+        </div>
+
+    </header>
+
+
+    <section id="showcase">
+
+        <div class="container">
+            <img id="showcaselogo" src="images/MainLogo1.png">
+            <!-- <h1>THE ROAD RUNNERS</h1> -->
+            <p>VIT Chennai's Official BAJA Team</p>
+        </div>
+
+    </section>
+
+
+    <footer>
+
+        <section id="socialmedia">
+
+            <div class="container">
+                <h1>Links</h1>
+                <p>CREATED BY Wile E. Coyote </p>
+            </div>
+
+        </section>
+
+
+    </footer>
+
+
+
+</body>
+
+</html>
